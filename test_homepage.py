d47cfde9280597ead47dd23b6a5216647a78b24f
skku <127391061+qwawa@users.noreply.github.com>
2023-05-22 13:35:32 +0900

    Update test_homepage.py

--- test_homepage.py
+++ test_homepage.py
@@ -3,6 +3,6 @@ import requests
 def test_homepage_load():
     url = "https://qwawa.github.io"
 
-    response = requests.get(url)
-    assert response.status_code == 200
-    assert 'qwawa.github.io' in response.text
+    response = requests.get(url) # Send a GET request to the specified URL
+    assert response.status_code == 200 # Check if the response status code is 200
+    assert 'qwawa.github.io' in response.text # Check if 'qwawa.github.io' is present in the response text
